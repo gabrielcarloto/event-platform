c2d2328b662ee8d40cf84d8c5db3ab12c11598fc
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-06-22 16:56:43 -0300

    ✨ feat: add Sidebar

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,5 +1,14 @@
 import Header from '../components/Header';
+import Player from '../components/Player';
+import Sidebar from '../components/Sidebar';
 
 export default function Event() {
-  return <Header />;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        <Sidebar />
+      </main>
+    </div>
+  );
 }
