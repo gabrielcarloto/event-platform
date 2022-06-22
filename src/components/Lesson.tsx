17482977352f83f8cb0c9f04b5d2a69a0da1e391
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-06-22 20:33:02 -0300

    ✨ feat: add hover animation to Lesson component

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -23,10 +23,10 @@ export default function Lesson({
   );
 
   return (
-    <a href="#">
+    <a href="#" className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
